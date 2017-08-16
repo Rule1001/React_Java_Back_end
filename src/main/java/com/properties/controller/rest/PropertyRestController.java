f362c339570c33e01066c0cf9491eb035c4fe17e
Rule1001 <hello@rule1001.com>
2017-08-16 17:24:50 +0100

    added fetch property by id method to rest controller

--- src/main/java/com/properties/controller/rest/PropertyRestController.java
+++ src/main/java/com/properties/controller/rest/PropertyRestController.java
@@ -21,5 +21,12 @@ public class PropertyRestController {
     @RequestMapping(value="/properties", method = RequestMethod.GET)
     public List<Property> getAllProperties() { return (List<Property>)propertyRepository.findAll(); }
 
+    @CrossOrigin("*")
+    @RequestMapping(value="/properties/{propertyId}", method = RequestMethod.GET)
+    public Property getIndividulaProperty(@PathVariable Long propertyId) {
+        Property property = propertyRepository.findOne(propertyId);
 
+        return property;
+    }
+    
 }
