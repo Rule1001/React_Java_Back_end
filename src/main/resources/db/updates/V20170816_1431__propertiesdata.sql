a954646fa1f1cf4449d49fbae3d204d5d2c2a264
Rule1001 <hello@rule1001.com>
2017-08-16 17:12:11 +0100

    initial data loaded

---
+++ src/main/resources/db/updates/V20170816_1431__propertiesdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO property VALUES (1, 'detached', 4, 'Sale');
+INSERT INTO property VALUES (2, 'semi-detached', 3, 'Altrincham');
+INSERT INTO property VALUES (3, 'Apartments', 1, 'Manchester');
